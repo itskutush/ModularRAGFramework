65a39db90339cb5cfcaf1399430fbea252e41221
user@10 <user10@admin10s-MacBook-Pro-2.local>
2026-01-03 00:00:59 +0530

    dataset

---
+++ ingestion_pipeline.py
@@ -0,0 +1,20 @@
+'''Ingestion pipeline'''
+import os 
+from langchain_community.document_loaders import TextLoader , DirectoryLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+
+
+def main():
+    print("Main Function")
+
+    
+if __name__ == "__main__":
+    main()
