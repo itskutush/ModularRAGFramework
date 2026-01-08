3d67a03a105d3dcc4b1f9e7347b8eb997ddfdf02
user@10 <user10@admin10s-MacBook-Pro-2.local>
2026-01-08 22:52:14 +0530

    updated ingestion

--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -9,11 +9,28 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+def load_documentts(docs_path = "docs"):
+    loader = DirectoryLoader(docs_path, glob="**/*.txt", loader_cls=TextLoader)
+    documents = loader.load()
 
+    for i , doc in enumerate(documents[:2]):
+        print(f"Document {i+1} :")
+        print(f" Source: {doc.metadata['source']}")
+        print(f" Content length: {len(doc.page_content)} characters")
+        print(f" Content preview: {doc.page_content[:100]}...\n")
+        print(f" metadata: {doc.metadata}\n")
+        
+    return documents
 
 
 def main():
     print("Main Function")
+
+    #1. Load Documents
+    documents = load_documentts(docs_path="docs")
+
+    #2. chunk files
+    #3. embedding and storing vector db
 
     
 if __name__ == "__main__":
